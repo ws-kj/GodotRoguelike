c29cf193b89c2fc9d9eea4365f3bfe5fa4dc500d
Will Savage <wsavage6316@gmail.com>
2019-11-27 10:25:03 -0500

    added more features to arrow

--- scripts/Arrow.gd
+++ scripts/Arrow.gd
@@ -5,11 +5,12 @@ onready var player = get_parent().get_node("Player")
 onready var anim = $AnimationPlayer
 onready var light = $Light2D
 
-export var fallen = false
-export var active = true
-export var damage = 2
+var fallen = false
+var active = true
 
+export var damage = 2
 export var attack_speed = 0.5
+export var view_range = 400
 
 var can_attack = true
 
@@ -23,6 +24,16 @@ func _physics_process(delta):
 	if active:
 		if not fallen:
 			move()
+			check_fall()
+			
+func check_fall():
+	if abs(get_global_mouse_position().x - global_position.x) < 3 and \
+		abs(get_global_mouse_position().y - global_position.y) < 3:
+		fall()
+		
+	if abs(player.global_position.x - global_position.x) > view_range and \
+		abs(player.global_position.y - global_position.y) > view_range:
+			fall()
 			
 func move():
 	vel = Vector2()
@@ -36,7 +47,10 @@ func _on_Area2D_body_entered(body):
 			body.take_damage(damage)
 			can_attack = false
 			anim.play("Inactive")
+			light.hide()
 			yield(get_tree().create_timer(attack_speed), "timeout")
+			anim.play("Active")
+			light.show()
 			can_attack = true
 		elif body.is_in_group("Player"):
 			deactivate(body)
@@ -47,7 +61,7 @@ func fall():
 	fallen = true
 	can_attack = false
 	light.hide()
-	#anim.play("Fallen")
+	anim.play("Fallen")
 	
 func activate():
 	active = true
--- scripts/Quiver.gd
+++ scripts/Quiver.gd
@@ -7,6 +7,8 @@ onready var start_position = $Sprite/StartPosition
 var has_arrow = true
 var arr
 
+var check_pickup = false
+
 func attack():
 	if has_arrow:
 		arr = arrow.instance()
@@ -14,3 +16,12 @@ func attack():
 		arr.rotation = start_position.rotation
 		has_arrow = false
 		get_tree().get_current_scene().add_child(arr)
+		check_pickup  = false
+		yield(get_tree().create_timer(1), "timeout")
+		check_pickup = true
+
+
+func _on_Area2D_area_entered(body):
+	print(body)
+	if body.get_parent() == arr and check_pickup:
+		body.get_parent().deactivate(get_parent().get_parent())
